bc6ea6c006d1114a066f279c417ecd78a8fd0df9
Dananjay Srinivas <dasr8731@colorado.edu>
2022-01-07 19:03:06 +0000

    Fixed Nginx bug

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,6 @@ python3 manage.py migrate
 python3 manage.py collectstatic --no-input 
 python3 manage.py shell < /docker/set_domain_name.py  
 
-nginx -g daemon off 
-pid /tmp/nginx.pid
+nginx -c /docker/nginx.conf -g "pid /tmp/nginx.pid;"
 
-uwsgi --socket :8000 --master --enable-threads --module config.wsgi
+uwsgi --socket config.sock --master --enable-threads --module config.wsgi
