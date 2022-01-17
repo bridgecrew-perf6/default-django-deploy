51c1771b0779d189c201dec0ca3883d411b09188
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2022-01-16 18:13:32 -0700

    First commit for example. Server and example services not committed.

--- src/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from config import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.default_view, name='default')
-]
--- src/config/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def default_view(request) :
-    return render(request, 'index.html')
